eb1faefa199addd62a9f2f936cb848d8c9be5764
alejandro-sanchez1992 <33785236+alejandro-sanchez1992@users.noreply.github.com>
2023-04-15 14:40:36 -0500

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class App {
   name = 'Juego Piedra, Papel, Tijera';
@@ -15,6 +16,19 @@ export class App {
   resultado: string;
   puntosUsuario = 0;
   puntosComputadora = 0;
+
+  constructor(private playGame: GameService) {}
+
+  ngOnInit(): void {
+    this.resultado = 'Esperando Jugada ...';
+  }
+
+  play(choice: string): void {
+    const result = this.playGame.game(choice);
+    this.resultado = result.message;
+    this.puntosUsuario += result.userAdd;
+    this.puntosComputadora += result.compAdd;
+  }
 }
 
 bootstrapApplication(App);
